1174defad66f030c0eb2084082e223145830e0b6
Arun <121193122+arundodamani27@users.noreply.github.com>
2026-07-12 12:04:03 +0530

    Update sitemap URL from vercel.app to arundodamani.tech

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://arundodamani.vercel.app",
+      url: "https://arundodamani.tech",
       lastModified: new Date(),
     },
   ];
